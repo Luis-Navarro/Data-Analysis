d74248a935439a2a288847d50b2487519983c420
Luis-Navarro <36213637+Luis-Navarro@users.noreply.github.com>
2018-02-07 00:30:58 +0000

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,14 @@
+# Luis Navarro, 06-Feb-2018
+# Collatz Conjeture
+
+i = int (input ("Please enter a starting integer for testing Collatz Conjeture: "))
+j = 0
+while (i != 1):
+    if (i % 2 == 0):
+        i = i/2
+    elif (i % 2 != 0):
+        i = i*3 + 1
+    print (i)
+    j = j+1
+
+print ("It took "),  (j),  ("steps to get to 1")
